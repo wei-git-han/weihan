10e8d95ed4c6f1750e7605a51dd91be113b67c65
wei-git-han <18303010235@163.com>
2020-03-10 21:57:35 +0800

    2155

--- src/main/java/com/test/DiGuiTwo.java
+++ src/main/java/com/test/DiGuiTwo.java
@@ -3,6 +3,7 @@ package com.test;
 public class DiGuiTwo {
     public static void main(String[] args){
         int i=5;
+        //
         int sum = sub(i);
         System.out.println(sum);
     }
